0e390d35a80b0448ba0ba9806503e9bb3c65cfd5
zobeir-rigi <rigi.zobir@yahoo.com>
2023-10-10 02:50:12 +0100

    screens size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: { 
+      screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+  },
   },
   plugins: [],
 }
